43fb7ae55682e587765ce695692eff0e5acf8ce0
Greg Bougeard <gbougeard@gmail.com>
2013-01-15 20:34:55 +0100

    Create a 4 steps wizards

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,13 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/wizard/step1', {templateUrl: 'partials/wizard/step1.html', controller: 'WizStep1Controller'});
+    $routeProvider.when('/wizard/step2', {templateUrl: 'partials/wizard/step2.html', controller: 'WizStep2Controller'});
+    $routeProvider.when('/wizard/step3', {templateUrl: 'partials/wizard/step3.html', controller: 'WizStep3Controller'});
+    $routeProvider.when('/wizard/step4', {templateUrl: 'partials/wizard/step4.html', controller: 'WizStep4Controller'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,46 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
+function MyCtrl1() {
+}
 MyCtrl1.$inject = [];
 
 
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+myApp.controller('WizStep1Controller',
+    function ($scope, $location) {
+
+        $scope.next = function () {
+            $location.path('/wizard/step2');
+        };
+
+    });
+myApp.controller('WizStep2Controller',
+    function ($scope, $location) {
+
+        $scope.back = function () {
+            $location.path('/wizard/step1');
+        };
+        $scope.next = function () {
+            $location.path('/wizard/step3');
+        };
+
+    });
+myApp.controller('WizStep3Controller',
+    function ($scope, $location) {
+
+        $scope.next = function () {
+            $location.path('/wizard/step4');
+        };
+        $scope.back = function () {
+            $location.path('/wizard/step2');
+        };
+
+    });
+myApp.controller('WizStep4Controller',
+    function ($scope, $location) {
+
+        $scope.back = function () {
+            $location.path('/wizard/step3');
+        };
+
+    });
