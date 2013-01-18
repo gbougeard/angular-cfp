ace1f1edc54c6c9fb0e7482234f073fe23665703
Greg Bougeard <gbougeard@gmail.com>
2013-01-18 18:55:52 +0100

    Create fromNow filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -3,8 +3,13 @@
 /* Filters */
 
 angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+    filter('interpolate', ['version', function (version) {
+        return function (text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
+    }]).
+    filter('fromNow', function () {
+        return function (dateString) {
+            return moment(new Date(dateString)).fromNow();
+        }
+    });
